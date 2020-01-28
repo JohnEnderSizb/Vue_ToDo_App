0dbb0f2f69cce352181c1f443c79383fe14f4938
Johnson A.Siziba <siziba.uz@gmail.com>
2020-01-28 15:40:09 +0200

    style add todo

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,13 +1,27 @@
 <template>
-
+    <div>
+        <form>
+            <input type="text" name="totle" placeholder="Add todo...">
+            <input type="submit" value="submit" class="btn">
+        </form>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "AddTodo.vue"
+        name: "AddTodo"
     }
 </script>
 
 <style scoped>
-
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex: 2;
+    }
 </style>
